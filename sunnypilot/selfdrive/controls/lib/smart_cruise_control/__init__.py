9808cce257251563223e794a9922868e5f6306df
mac-858 <mac773@att.net>
2026-08-17 18:18:59 -0700

    Add v_target_smoothed variable for cruise control

--- sunnypilot/selfdrive/controls/lib/smart_cruise_control/__init__.py
+++ sunnypilot/selfdrive/controls/lib/smart_cruise_control/__init__.py
@@ -7,3 +7,5 @@ See the LICENSE.md file in the root directory for more details.
 from openpilot.common.constants import CV
 
 MIN_V = 20 * CV.KPH_TO_MS  # Do not operate under 20 km/h
+
+self.v_target_smoothed = V_CRUISE_UNSET
